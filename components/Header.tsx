705c9c5d301006bd0a494474052e45af126a3cfb
Grzegorz 'Hankier' Ćwikliński <hankier@hqr.sh>
2024-04-26 14:37:48 +0200

    Add initial version

---
+++ components/Header.tsx
@@ -0,0 +1,47 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Image from './Image'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+import SearchButton from './SearchButton'
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between py-10">
+      <div>
+        <Link href="/" aria-label={siteMetadata.headerTitle}>
+          <div className="flex items-center justify-between">
+            <div className="mr-3">
+              <Image
+                alt="ChcęZostać logo"
+                src="/static/images/logo.png"
+                className="object-cover object-center"
+                width={171}
+                height={126}
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+        {headerNavLinks
+          .filter((link) => link.href !== '/')
+          .map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+            >
+              {link.title}
+            </Link>
+          ))}
+        <SearchButton />
+        <ThemeSwitch />
+        <MobileNav />
+      </div>
+    </header>
+  )
+}
+
+export default Header
