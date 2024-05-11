1862467e6473943860698a4447aaa3f396df4009
Grzegorz 'Hankier' Ćwikliński <hankier@hqr.sh>
2024-05-11 09:55:46 +0200

    Style main page

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,7 +9,7 @@ const MAX_DISPLAY = 5
 export default function Home({ posts }) {
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700 w-full lg:w-4/5 mx-auto px-6 lg:px-0 pt-4 lg:pt-12">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Najnowsze wpisy 
@@ -27,7 +27,7 @@ export default function Home({ posts }) {
                 <article>
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                     <dl>
-                      <dt className="sr-only">Published on</dt>
+                      <dt className="sr-only">Opublikowano</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                       </dd>
@@ -56,10 +56,10 @@ export default function Home({ posts }) {
                       <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          className="text-cz-magenta hover:text-cz-pink dark:hover:text-cz-pink"
                           aria-label={`Read more: "${title}"`}
                         >
-                          Read more &rarr;
+                          Czytaj więcej &rarr;
                         </Link>
                       </div>
                     </div>
