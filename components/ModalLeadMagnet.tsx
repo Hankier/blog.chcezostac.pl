d5802630a75f79b7baaa8ca2d1236675f71ff9e8
Grzegorz Ćwikliński <dev@hqr.sh>
2024-05-20 15:53:25 +0200

    Fix Modal

--- components/ModalLeadMagnet.tsx
+++ components/ModalLeadMagnet.tsx
@@ -75,25 +75,26 @@ const ModalLeadMagnet = ({ title, subtitle, imageSrc, lm_list }) => {
 
     return (
         <div className="fixed inset-0 bg-cz-bg-dark bg-opacity-70 flex justify-center items-center">
-            <div className="bg-white rounded-lg w-4/5 lg:w-1/2 p-px bg-gradient-to-r from-cz-blue to-cz-pink ">
-                <div className="bg-cz-bg-dark p-5 rounded-lg w-full">
+            <div className="bg-white rounded-lg w-5/6 lg:w-1/2 p-px bg-gradient-to-r from-cz-blue to-cz-pink ">
+                <div className="bg-cz-bg-dark p-2 lg:p-5 rounded-lg w-full">
                     <button onClick={handleClose} className="absolute top-4 right-4 text-2xl">
                         &times;
                     </button>
                     <div>
                     <h2 className="text-medium lg:text-xl text-center">
-                    <span className="font-bold bg-clip-text text-xl lg:text-4xl text-transparent bg-gradient-to-r from-cz-blue to-cz-pink">{subtitle}</span></h2>
+                    <span className="font-bold bg-clip-text text-xl lg:text-4xl text-transparent bg-gradient-to-r from-cz-blue to-cz-pink">{title}</span>
+                    </h2>
                     </div>
-                    <div className="flex flex-col md:flex-row items-center justify-center gap-4 mt-4">
-                        <div className="w-full lg:w-2/3 text-sm lg:text-base">
-                            <span dangerouslySetInnerHTML={{ __html: title }} />
+                    <div className="flex flex-col md:flex-row items-center justify-center gap-4 mt-2 lg:mt-4">
+                        <div className="w-full lg:w-2/3 text-sm lg:text-base list-disc px-2">
+                            <span dangerouslySetInnerHTML={{ __html: subtitle }} />
                         </div>
                     <div className="w-full lg:w-1/3 items-center">
                         <img src={imageSrc} alt="Modal Image" className="hidden lg:block object-cover w-1/2 mx-auto py-4" />
                         <div className="flex flex-col items-center">
                             <form onSubmit={handleSubmit} className="flex flex-col items-center">
                                 <input 
-                                    className="text-white mx-4 font-bold text-lg py-2 px-2 rounded bg-cz-bg-dark border border-gray-200 focus:border-cz-magenta focus:ring-cz-magenta" placeholder="Email"
+                                    className="text-white mx-4 font-bold text-sm lg:text-lg py-2 px-2 rounded bg-cz-bg-dark border border-gray-200 focus:border-cz-magenta focus:ring-cz-magenta" placeholder="Email"
                                     value={input}
                                     onChange={e => setInput(e.target.value)}
                                     placeholder="jan@kowalski.pl"
@@ -109,7 +110,7 @@ const ModalLeadMagnet = ({ title, subtitle, imageSrc, lm_list }) => {
                                 Odbieram!
                                 </button>
                             </form>
-                            <div className="mt-2 text-sm text-gray-400 w-full text-center mx-auto">
+                            <div className="mt-2 text-xs lg:text-sm text-gray-400 w-full text-center mx-auto">
                                 Zapoznałem się z <Link href="https://panel.chcezostac.pl/podstrona/1/regulamin.html" className="underline hover:text-cz-purple">Regulaminem</Link> oraz <Link href="https://panel.chcezostac.pl/podstrona/2/politykaprywatnosci.html" className="underline hover:text-cz-purple">Polityką Prywatności</Link> i akceptuję ich postanowienia. Wypełniając formularz, wyrażasz zgodę na otrzymywanie wartościowych treści w newsletterze od "Chcę Zostać". W każdej chwili możesz się z niego wypisać.*
                             </div>
                         </div>
